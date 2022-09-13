0c9f1451f6e06a8eba0c263786393f8afc8eebef
Despair <akmashev2002@gmail.com>
2022-09-13 20:27:37 +0300

    Создание матриц

---
+++ cgLabs/MatrixLib/Matrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgLabs.MatrixLib
+{
+    public class Matrix
+    {
+        int Size { get; set; }
+        public Matrix() { }
+    }
+}
